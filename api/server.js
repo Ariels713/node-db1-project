fc65d3f44318bde80fd6c85dfb28a0cc7722f702
Ariel_Rodriguez <32606708+Ariels713@users.noreply.github.com>
2020-06-16 21:27:36 -0400

    initial commit

--- api/server.js
+++ api/server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const db = require("../data/dbConfig.js");
 
 const server = express();
-
+server.use(express.urlencoded({extended: true}))
 server.use(express.json());
 
 module.exports = server;
